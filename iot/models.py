5c9fc5b8cd79c34ceedde2d71d12a9e7ecea8ca4
massmr <marcellin.massimo@gmail.com>
2025-03-26 11:47:01 +0100

    feat : add some data in model

--- iot/models.py
+++ iot/models.py
@@ -4,12 +4,13 @@ class Devices(models.Model):
     name = models.CharField(max_length=100)
     createdAt = models.DateTimeField(auto_now_add=True)
     lastSeenAt = models.DateTimeField(auto_now=True)
+    ip = models.GenericIPAddressField()
+    uid = models.CharField(max_length=100)
 
 class Temperatures(models.Model):
     device = models.ForeignKey(Devices, on_delete=models.CASCADE, related_name='temperatures')
     value = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
-        return f"{self.name} ({self.uid})"
+        return f"{self.device} : {self.value}°C at {self.timestamp}"
