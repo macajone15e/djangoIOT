c8548c128a2f9a54ee5ad778206e243a25f3a32c
massmr <marcellin.massimo@gmail.com>
2025-03-30 21:06:47 +0200

    feat : add some lines to device model

--- iot/models.py
+++ iot/models.py
@@ -7,11 +7,15 @@ class Devices(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     lastSeenAt = models.DateTimeField(auto_now=True)
     ip = models.GenericIPAddressField()
+    room = models.CharField(max_length=100)
     version = models.CharField(max_length=100)
-
+    attach = models.PositiveIntegerField(default=0)
+    platform = models.ForeignKey('Platform', on_delete=models.CASCADE, related_name='devices')
+    otaPassword = models.CharField(max_length=100)
+    sensorDelay = models.PositiveIntegerField(default=0)
+    rssi = models.PositiveIntegerField(default=0)
     def __str__(self):
         return f"{self.name}, {self.uid}"
-    
     class Meta : 
         verbose_name = "Device"
         verbose_name_plural = "Devices"
@@ -20,10 +24,8 @@ class Temperatures(models.Model):
     device = models.ForeignKey(Devices, on_delete=models.CASCADE, related_name='temperatures')
     value = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"{self.device} : {self.value}°C at {self.timestamp}"
-    
     class Meta :
         verbose_name = "Temperature"
         verbose_name_plural = "Temperatures"
